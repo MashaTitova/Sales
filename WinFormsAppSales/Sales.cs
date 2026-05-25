c79ecf535b9c9df6a6aa2a7bc25928a4caf5ebba
MashaTitova <masha.m.titova@yandex.ru>
2026-05-25 15:36:37 +0300

    0

---
+++ WinFormsAppSales/Sales.cs
@@ -0,0 +1,15 @@
+namespace WinFormsAppSales
+{
+    public partial class Form_Sales : Form
+    {
+        public Form_Sales()
+        {
+            InitializeComponent();
+        }
+
+        private void Sales_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
